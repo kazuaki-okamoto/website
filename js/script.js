68bee7d55e1105a8e3ab048edc1d32442c48c568
kazuaki-okamoto <kanki995417@gmail.com>
2022-11-11 14:17:26 +0900

    最初のコミット

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(function(){
+    $(".catchcopy h1").dblclick(
+        function(){
+            $(".catchcopy h1").text("非日常に日常を");
+    });
+});
+
+$(function(){
+    $(".menu1").hover(function(){
+        $(".menu1").css("color","yellow");
+    },
+    function(){
+        $(".menu1").css("color","white");
+    });
+});
+
+$(function(){
+    $(".menu2").hover(function(){
+        $(".menu2").css("color","yellow");
+    },
+    function(){
+        $(".menu2").css("color","white");
+    });
+});
+
+$(function(){
+    $(".menu3").hover(function(){
+        $(".menu3").css("color","yellow");
+    },
+    function(){
+        $(".menu3").css("color","white");
+    });
+});
+
+$(function(){
+    $(".menu4").hover(function(){
+        $(".menu4").css("color","yellow");
+    },
+    function(){
+        $(".menu4").css("color","white");
+    });
+});
+
+/*$(function(){
+    $(".news1").hover(function(){
+        $(this).animete({width:"300px"});
+    },
+    function(){
+        $(this).animete({width:"250px"});
+    });
+});*/
+
+
+
+
